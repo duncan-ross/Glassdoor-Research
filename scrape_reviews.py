08c1578e5e8e370d39bdf0443aeb52832f74d064
duncan-ross <duncanross.eng@gmail.com>
2022-12-23 01:50:52 -0500

    Working scrape for all review info. Missing main text fields and star sub-sections.

--- scrape_reviews.py
+++ scrape_reviews.py
@@ -5,10 +5,10 @@ import json
 import re
 import sys
 import time
-from math import ceil
+import math
 from helpers import json_data
 from get_review_urls import updateCompaniesMap
-
+from dateutil.parser import parse
 
 # Check if there are any new companies in the companies map to be scraped
 def main(fetchAllCompanies=False):
@@ -35,7 +35,9 @@ def main(fetchAllCompanies=False):
     # Fetch all reveiws (company_names currently contains 01/02/10-05/31/17)
     for i, (name, info) in enumerate(new_companies_map.items()):
         start = time.time()
-        print("{}/{}: Finding reviews for {}".format(i+1, len(new_companies_map), name))
+        print(
+            "{}/{}: Finding reviews for {}".format(i + 1, len(new_companies_map), name)
+        )
         reviewsUrl = info["reviews_url"]
 
         num_pages = get_num_pages(reviewsUrl)
@@ -64,45 +66,37 @@ def main(fetchAllCompanies=False):
 
     print("TOTAL REVIEWS FETCHED: {}".format(numReviewsFetched))
 
+
+# Returns the beautiful soup html content for a given url
+def get_html_body(url):
+    sauce = requests.get(url, headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(sauce.content, "html.parser")
+    return soup
+
+
 # Gets the number of pages of reviews per company by finding the total number of reviews and then dividing by number of reviews per page (10)
 def get_num_pages(url):
-  sauce = requests.get(url, headers=DEFAULT_HEADERS)
-  soup = BeautifulSoup(sauce.content,'html.parser')
-
-  numReviewsString = soup.find("div", {"class": "paginationFooter"}).text
-  print(numReviewsString)
-  result = numReviewsString[numReviewsString.find("of"):]
-  res = max([int(i) for i in numReviewsString.split() if i.isdigit()])
-  print(res)
-
-
-
-
-
-
-
-
-    
+    body = get_html_body(url)
+    numReviewsString = body.find("div", {"class": "paginationFooter"}).text
+    res = max([int(i) for i in numReviewsString.split() if i.isdigit()])
+    return math.ceil(res / 10)
 
 
 # Get the html from a page of reviews (e.g. page 3 of the reviews for Secure Works)
 def fetch_review_page_data(url, page_num):
     if page_num > 1:
-        # Old code
-        #   url = '{}_P{}.htm'.format(url.rstrip('.htm'), page_num)
-        # New code that handles adding freelancers, contractors, & interns. Glassdoor started auto-filtering for just full-time and part-time employees
         url = url.replace(".htm", "_P" + str(page_num) + ".htm")
         print("Page " + str(page_num))
 
     body = get_html_body(url)
 
     if not body:
-        return []
+        return False
 
     reviews_feed = body.find_all(attrs={"class": "emp-reviews-feed"})
 
     if not reviews_feed:
-        return
+        return False
 
     reviews = reviews_feed[0].find_all("li", attrs={"class": "empReview"})
 
@@ -117,21 +111,36 @@ def fetch_review_page_data(url, page_num):
 
 
 # Scrape the review info from the html from a single review
-def scrape_review_info(el):
+def scrape_review_info(body):
     review = {}
 
     # Get the text information
-    for k, info in class_details_map().items():
+    parse_text_information(body, review)
+
+    # Get the overall star rating
+    parse_star_rating(body, review)
+
+    # Get the date the review was posted
+    parse_date_role_location(body, review)
+
+    # Get the employment status and tenure of the reviewer
+    parse_current_employment_status_tenure(body, review)
+
+    return review
+
+
+def parse_text_information(soup, review):
+      for k, info in class_details_map().items():
         tag_name = info["tag_name"]
         classes = info["classes"]
 
         if len(classes) > 1:
-            text_el = el.select("{}.{}".format(tag_name, ".".join(classes)))
+            text_el = soup.select("{}.{}".format(tag_name, ".".join(classes)))
 
             if text_el:
                 text_el = text_el[0]
         else:
-            text_el = el.find(tag_name, attrs={"class": classes})
+            text_el = soup.find(tag_name, attrs={"class": classes})
 
         if text_el:
             text = text_el.text
@@ -140,58 +149,20 @@ def scrape_review_info(el):
                 text = text.strip('"')
 
             review[k] = text
-
-    # Get the overall star rating
-    stars = find_star_rating(el)
-
-    if stars:
-        review["stars"] = stars
-
-    # Get the date the review was posted
-    post_date = find_post_date(el)
-
-    if post_date:
-        review["post_date"] = post_date
-
-    # Get the job title and status of the reviewer
-    job_title_status_info = get_job_title_and_status(el)
-
-    if job_title_status_info:
-        review["job_title"] = job_title_status_info["job_title"]
-        review["current_employee"] = job_title_status_info["current_employee"]
-
-    # Get the reccomendation, outlook, and CEO approval
-    approval_info = get_approval_info(el)
-
-    if approval_info:
-        review["approval_info"] = approval_info
-
-    # Get the amount of time an employee has been working at the company
-    emp_status_duration_info = get_emp_status_and_duration(el)
-
-    if emp_status_duration_info.get("employment_status"):
-        review["employment_status"] = emp_status_duration_info["employment_status"]
-
-    if emp_status_duration_info.get("years_employed"):
-        review["years_employed"] = emp_status_duration_info["years_employed"]
-
-    return review
+      return
 
 
 # Finds and retunrs the overall star rating of the company as well as the breakdown in star rating between culture & values, work/life balance, senior management, comp & benefits, and career opportunities
-def find_star_rating(el):
+def parse_star_rating(soup, review):
     ratings = {}
-    overall_stars_el = el.find("span", attrs={"class": "value-title"})
+    overallRating = soup.find("span", attrs={"class": "ratingNumber"}).text
 
-    if (
-        not overall_stars_el
-        or not overall_stars_el.attrs
-        or not overall_stars_el.attrs.get("title")
-    ):
+    if not overallRating:
         return None
 
-    ratings["overall"] = parse_star_val(overall_stars_el)
-
+    #ratings["overall"] = overallRating
+    review["overall_rating"] = overallRating
+    return
     sub_ratings_parent = el.select(".subRatings.module")
 
     if not sub_ratings_parent:
@@ -219,13 +190,44 @@ def find_star_rating(el):
 
 
 # Finds and returns the date that a review was posted
-def find_post_date(el):
-    date_el = el.find("time", attrs={"class": "date"})
+def parse_date_role_location(soup, review):
+    jobLine_el = soup.find(
+        "span", attrs={"class": "common__EiReviewDetailsStyle__newUiJobLine"}
+    )
+    jobLine_arr = [
+        s.strip()
+        for s in jobLine_el.find(
+            "span", attrs={"class": "common__EiReviewDetailsStyle__newGrey"}
+        ).text.split("-")
+    ]
+    date, role = jobLine_arr[0], jobLine_arr[1]
+    date = parse(date).strftime("%m/%d/%Y")
+    location = None
+
+    try:
+        location = (
+            (jobLine_el.findAll("span", attrs={"class": "middle"})[1]).find("span").text
+        )
+    except (IndexError):
+        pass
+    
+    review["date"] = date
+    review["job_title"] = role
+    review["location"] = location
+    return
+
+
+"""
+    if val and len(val.split("-")) == 2:
+        status, job_title = [s.strip() for s in val.split("-")]
+        info["current_employee"] = "current" in status.lower()
+        info["job_title"] = job_title
 
     if not date_el or not date_el.attrs or not date_el.attrs.get("datetime"):
         return None
 
     return date_el.attrs["datetime"]
+    """
 
 
 # Returns the value of the 'title' class within an element
@@ -234,17 +236,32 @@ def parse_star_val(el):
 
 
 # Finds and returns the job title and status of the employee posting a review
-def get_job_title_and_status(el):
-    job_title_status_el = el.find("span", attrs={"class": "authorJobTitle"})
-    val = job_title_status_el.text
+def parse_current_employment_status_tenure(soup, review):
+    jobStatusTenure = soup.find(
+        "span", attrs={"class": "pt-xsm pt-md-0 css-1qxtz39 eg4psks0"}
+    ).text
+    current_employee = "current" in jobStatusTenure.lower()
 
-    info = {}
-    if val and len(val.split("-")) == 2:
-        status, job_title = [s.strip() for s in val.split("-")]
-        info["current_employee"] = "current" in status.lower()
-        info["job_title"] = job_title
+    employment_types = ["full-time", "part-time", "contract", "intern", "freelance"]
+    type_arr = [val in jobStatusTenure for val in employment_types]
+    employment_status = type_arr[0] if type_arr[0] else None
 
-    return info
+    try:
+        tenure_text = jobStatusTenure.lower().split(",")[1]
+        years = str(max([int(i) for i in tenure_text.split() if i.isdigit()]))
+        if "less" in tenure_text:
+            tenure = "<" + years
+        elif "more" in tenure_text:
+            tenure = ">" + years
+        else:
+            tenure = years
+    except(IndexError):
+      tenure = None
+    
+    review["current_employee"] = current_employee
+    review["years_employed"] = tenure
+    review["employment_status"] = employment_status
+    return
 
 
 # Finds and returns the approval info in three categories: recommend to a friend, approves of CEO, and company outlook
@@ -368,5 +385,5 @@ def write_reviews_to_file(reviewsData):
 
 if __name__ == "__main__":
     force = "--force" in sys.argv
-    #main(fetchAllCompanies=force)
+    # main(fetchAllCompanies=force)
     main(fetchAllCompanies=True)
